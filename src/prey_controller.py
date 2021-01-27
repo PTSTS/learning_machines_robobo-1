966bd4c62bd28c9e1d39b599b3dc5ce97f287e44
Peter Santos <AIMEPTSTSAM@mail.com>
2021-01-27 05:31:25 +0100

    Create prey_controller.py

---
+++ src/prey_controller.py
@@ -0,0 +1,16 @@
+from prey import *
+from controller import Controller
+
+class prey_controller_nn(Prey):
+    def __init__(self, gene):
+        self.gene = gene
+        super(Prey, self).__init__()
+
+    def run(self):
+        c = Controller(self.gene, 8, 2, 2)
+
+        while not self.stopped():
+            sensors = self._robot.read_irs()
+            sensors = [sensors[i] * 5 if sensors[i] is not False else 1 for i in range(len(sensors))]
+            left, right = c.act(sensors)
+            self._robot.move(left, right, millis=200)
